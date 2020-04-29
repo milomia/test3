61119d44646b4480d3afdcbde43e1557a9b51896
milomia <m-hunt3@sky.com>
2020-04-29 17:28:08 +0100

    remove breakpoint

--- app.py
+++ app.py
@@ -64,7 +64,6 @@ def exec_order_sql(sqlStr,sdate):
     end = end_date.isoformat().split('T')
     end = end[0]
     try:
-        breakpoint()
         db_path=os.path.join(basedir, 'suade.db')
         conn = sqlite3.connect(db_path)
         sql= f"from orders inner join orderline on orders.order_id = orderline.order_id and created_at BETWEEN '{start}' and '{end}'"
