dde7dfd4ef5e3c37ec20db1e82e59c5ee2c2a9a0
milomia <m-hunt3@sky.com>
2020-05-15 16:29:40 +0100

    split out calc to calc + run

--- calc.py
+++ calc.py
@@ -4,8 +4,3 @@ class Calc:
 
     def sub(self,a,b):
         return a -b
-
-c=Calc()
-x=c.add(5,7)
-y=c.sub(5,7)
-print(f"the diff between 5 and 7 is {y}")
---
+++ run.py
@@ -0,0 +1,6 @@
+from calc import Calc
+
+c=Calc()
+x=c.add(5,7)
+y=c.sub(5,7)
+print(f"the diff between 5 and 7 is {y}")
